e503b7cb77eb9bcf0a5ba386136261abb24e7c90
Trdat Mkrtchyan <zeffirsky@yandex-team.ru>
2016-04-01 19:43:51 +0300

    Freezed button

--- components/button/button.js
+++ components/button/button.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const Component = require('../Component');
+const get = require('lodash').get;
 
 module.exports = Component(function (params, data) {
     return {
-        text: params.text || data.text,
+        submit: params.submit ? 'type="submit"' : '',
+        text: get(data, params.text) || params.text,
     };
 });
---
+++ components/button/button.md
@@ -0,0 +1,12 @@
+# Button v0.1.0
+
+---
+
+Simple button
+
+### Options
+ - `tag` - default: `span` - Tag of button
+ - `text` - default: `"text"` - Text of button or way to text in page JSON
+ - `type` - type of button (for example submit)
+ - `className` - classes
+
